3866447449b186cd00f7a9bb21d399e7e505e4b4
pxluhor <pxluthor@gmail.com>
2024-04-22 15:52:02 -0300

    commit4

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+import sys
+
+sys.path.insert(1, "C:/past/your/coppied/path/here/streamlit_option_menu")
 import streamlit_authenticator as stauth
 
 import home, login, financeiro, suporte, comercial
 
-
+sys.path.insert(1, "C:/past/your/coppied/path/here/streamlit_option_menu")
 st.set_page_config(layout="wide", page_title="Leste Conecta")
 
 
