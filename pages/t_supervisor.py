ba49c2a0a443f7c905ebe67e83d637fb1709b469
pxluhor <pxluthor@gmail.com>
2024-04-28 16:38:57 -0300

    video

--- pages/t_supervisor.py
+++ pages/t_supervisor.py
@@ -8,10 +8,22 @@ import os
 st.set_page_config(layout="wide", page_title="Leste conecta")
 st.subheader(':green[LESTE CONECTA]', divider='rainbow')
 st.markdown(
-    '<h1 style="text-align: center; color: green;">TESTE DE ACESSO SUPERVSÃO</h1><br>', 
+    '<h1 style="text-align: center; color: green;">TESTE DE ACESSO SUPERVIÃO</h1><br>', 
             
             unsafe_allow_html=True)
 
  
 
 st.sidebar.link_button("Trancrição de Áudio", "https://transcriptionleste.streamlit.app/")
+
+st.title('Tetando vídeo')
+
+
+container = st.container(border=True)
+VIDEO_URL = "https://youtu.be/5aFnP22GpPI"
+
+
+#st.video(f'<iframe width="800" height="400" src="{st.video(VIDEO_URL)}" frameborder="0" allowfullscreen></iframe>', unsafe_allow_html=True)
+
+st.video(VIDEO_URL, format="video/mp4")
+#st.markdown(f'<iframe width="800" height="400" src="{st.video(VIDEO_URL)}" frameborder="0" allowfullscreen></iframe>', unsafe_allow_html=True)
