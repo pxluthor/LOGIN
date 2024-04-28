fe4441aef2383ff1c13b7c972c5377da43feba03
pxluhor <pxluthor@gmail.com>
2024-04-28 16:12:07 -0300

    Teste acesso a transcrição

--- pages/t_supervisor.py
+++ pages/t_supervisor.py
@@ -7,8 +7,11 @@ import os
 
 st.set_page_config(layout="wide", page_title="Leste conecta")
 st.subheader(':green[LESTE CONECTA]', divider='rainbow')
-st.markdown('<h1 style="text-align: center; color: green;">PROCEDIMENTOS - FINANCEIRO</h1><br>', unsafe_allow_html=True)
+st.markdown(
+    '<h1 style="text-align: center; color: green;">TESTE DE ACESSO SUPERVSÃO</h1><br>', 
+            
+            unsafe_allow_html=True)
 
  
 
-st.link_button("Trancrição de Áudio", "http://192.168.0.105:8501")
+st.sidebar.link_button("Trancrição de Áudio", "https://transcriptionleste.streamlit.app/")
